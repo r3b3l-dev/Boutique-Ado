28a75e7bd07bae0811dfcd89dfffcba23a110907
r3b3l-dev <wayne.manning@tuta.io>
2022-06-08 15:56:23 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L8MXsK0J8lkBATJiYhDr1hyE8Bs70iWVbnqKZYNE5h5BEf1aoRrhJQnRuRS1RfukTHcWet01CNkwFhKu3FU9xNG00FzueqXiL',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
